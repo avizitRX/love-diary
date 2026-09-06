d8e6be4c77a1dc2cef3e29736b25425447f626a2
avizitRX <contact@avizitrx.com>
2026-09-06 11:38:00 +0600

    chore: initial commit

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+
+export default async function Page() {
+  const cookieStore = await cookies();
+  const supabase = createClient(cookieStore);
+
+  const { data: todos } = await supabase.from("todos").select();
+
+  console.log("todos", todos);
+
+  return (
+    <ul>
+      {todos?.map((todo) => (
+        <li key={todo.id}>{todo.name}</li>
+      ))}
+    </ul>
+  );
+}
