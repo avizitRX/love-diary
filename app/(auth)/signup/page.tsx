1654a91304d1674b021c5b5ba58986ccd3c2755d
avizitRX <contact@avizitrx.com>
2026-09-09 18:26:23 +0600

    ui: login and signup

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,8 +1,7 @@
-import { LoginForm } from "@/components/login/login-form";
+import { SignupForm } from "@/components/signup/signup-form";
 import { GalleryVerticalEnd } from "lucide-react";
 
-
-export default function LoginPage() {
+export default function SignupPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
       <div className="flex w-full max-w-md flex-col gap-6">
@@ -12,7 +11,7 @@ export default function LoginPage() {
           </div>
           Acme Inc.
         </a>
-        <LoginForm />
+        <SignupForm />
       </div>
     </div>
   );
